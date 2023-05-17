a3a57542c164016363265ca40583d6cb9e5fbcee
danieljuma5 <jumadaniel275@gmail.com>
2023-05-17 22:42:45 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_accessor :title , :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
-
+book = Book.new("The day will come")
+book.author
+book.turn_page
